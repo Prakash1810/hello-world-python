e7b05a46e39c793c204e583003899c517789006f
Prakash-Devops <74908064+Prakash1810@users.noreply.github.com>
2026-08-12 17:22:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World class2 (Python)! (up %s)\n" % elapsed()
+    return "Hello World class3 updated version 2.0 (Python)! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
